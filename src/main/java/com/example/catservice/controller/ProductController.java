954aac733ddb1bffe0bfd1febb797f31ebf4b8cc
DanielMaku-STUDENT <g00467390@atu.ie>
2026-09-16 15:00:55 +0100

    added post and get for the product controller

--- src/main/java/com/example/catservice/controller/ProductController.java
+++ src/main/java/com/example/catservice/controller/ProductController.java
@@ -1,5 +1,26 @@
 package com.example.catservice.controller;
 
-public class ProductController {
+import com.example.catservice.model.Product;
+import com.example.catservice.service.ProductService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+@GetMapping
+    public List<Product> getProducts(){
+        return productService.getAll();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody  Product product){
+        return productService.create(product);
+    }
 }
