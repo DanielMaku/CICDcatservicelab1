e30b6c83e8bd0e55493276b3861606668a11db92
DanielMaku-STUDENT <g00467390@atu.ie>
2026-09-23 15:15:40 +0100

    finished

--- src/main/java/com/example/catservice/model/Product.java
+++ src/main/java/com/example/catservice/model/Product.java
@@ -21,13 +21,13 @@ public class Product {
     private String name;
     private BigDecimal price;
 
-    public Product() {}
+   // public Product() {}
 
-    public Product(Long id, String name, BigDecimal price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
+   // public Product(Long id, String name, BigDecimal price) {
+       // this.id = id;
+      //  this.name = name;
+     //   this.price = price;
+  //  }
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
