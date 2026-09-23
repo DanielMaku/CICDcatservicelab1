60adf8913cb6b6592d8ef1f52d438a2b6838c9ba
DanielMaku-STUDENT <g00467390@atu.ie>
2026-09-23 14:48:32 +0100

    created an interface so we can inherit all the JPA methods to write general crud actions. also added table to product.java

--- src/main/java/com/example/catservice/model/Product.java
+++ src/main/java/com/example/catservice/model/Product.java
@@ -1,10 +1,7 @@
 package com.example.catservice.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,12 +11,31 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 
+@Entity
+@Table(name = "Products")
 public class Product {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private BigDecimal price;
+
+    public Product() {}
+
+    public Product(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public BigDecimal getPrice() { return price; }
+    public void setPrice(BigDecimal price) { this.price = price; }
+
 }
